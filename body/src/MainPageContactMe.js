1e669c34b323c64d421b31cc4cc29f297e1e1608
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-12-18 04:06:21 +0800

    Add tele

--- body/src/MainPageContactMe.js
+++ body/src/MainPageContactMe.js
@@ -1,8 +1,18 @@
 import './MainPageContactMe.css';
 import github from './github.svg';
 import mail from './mail.png';
+import tele from './tele.svg';
 
 function MainPageContactMe() {
+
+  const copyToClipboardMail = () => {
+    navigator.clipboard.writeText("nigelyeotongwei@gmail.com");
+  }
+
+  const copyToClipboardTele = () => {
+    navigator.clipboard.writeText("@nigelyeo");
+  }
+
   return (
     <div className='contact-me-container'>
       <p className='lets-connect'> LET'S CONNECT </p>
@@ -10,9 +20,11 @@ function MainPageContactMe() {
         <div className='contact'>
           <img src={github} className='github'></img>
         </div>
-        <div className='contact'>
+        <div className='contact contact-tele' onClick={copyToClipboardTele}>
+          <img src={tele} className='tele'></img>
+          <p className='tele-text'>@nigelyeo</p>
         </div>
-        <div className='contact contact-mail'>
+        <div className='contact contact-mail' onClick={copyToClipboardMail}>
           <img src={mail} className='mail'></img>
           <p className='mail-text'>nigelyeotongwei@gmail.com</p>
         </div>
