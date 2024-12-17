6e49461fd58f2f544e8fbf843d5cae9cbb8c3458
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-12-18 03:50:18 +0800

    Add icons finish mail transitions

--- body/src/MainPageContactMe.js
+++ body/src/MainPageContactMe.js
@@ -1,9 +1,23 @@
 import './MainPageContactMe.css';
+import github from './github.svg';
+import mail from './mail.png';
 
 function MainPageContactMe() {
   return (
-    <>
-    </>
+    <div className='contact-me-container'>
+      <p className='lets-connect'> LET'S CONNECT </p>
+      <div className='contact-container'>
+        <div className='contact'>
+          <img src={github} className='github'></img>
+        </div>
+        <div className='contact'>
+        </div>
+        <div className='contact contact-mail'>
+          <img src={mail} className='mail'></img>
+          <p className='mail-text'>nigelyeotongwei@gmail.com</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
