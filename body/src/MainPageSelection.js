82572f19a39778d378a96794c989b921455fddce
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-12-18 21:45:02 +0800

    Touch up selection and animate

--- body/src/MainPageSelection.js
+++ body/src/MainPageSelection.js
@@ -1,12 +1,62 @@
 import './MainPageSelection.css';
+import react from 'react';
+import { useEffect, useRef } from 'react';
 
 function MainPageSelection() {
+
+  const targetRef = useRef(null);
+  
+  const options = {
+    root: null,
+    threshold: 0.65,
+    rootMargin: "0px",
+  };
+
+  let allJiggle = document.querySelectorAll('.select');
+
+  useEffect(() => {
+    allJiggle = document.querySelectorAll('.select');
+    const target = targetRef.current;
+    const theObserver = new IntersectionObserver(intersectionCallback, options);
+    if (target) {
+      theObserver.observe(target);
+    }
+
+    return () => {
+      if (target) {
+        theObserver.unobserve(target);
+      }
+    }
+  }, [])
+
+  const dummy = () => {
+
+  }
+
+  function intersectionCallback(entries) {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      allJiggle.forEach((jiggleEntry) => {
+        jiggleEntry.classList.add("jiggle");
+      })
+    } else {
+      allJiggle.forEach((jiggleEntry) => {
+        jiggleEntry.classList.remove("jiggle");
+      })
+    }
+  }
+
   return (
-    <div className="selection-overall">
-      <div className="projects select select-top">
+    <div ref={targetRef} className="selection-overall">
+      <div className="projects select">
+        <span>PROJECTS</span>
       </div>
-      <div className='awards select'></div>
-      <div className='contact-me select'></div>
+      <div className='work-awards select'>
+        <span>EXPERIENCE & AWARDS</span>
+      </div>
+      <div className='hobbies select'>
+        <span>HOBBIES</span>
+      </div>
     </div>
   )
 }
