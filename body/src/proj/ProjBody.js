1a3d00e5fb28c1f0f77201e58e60cce6f425966a
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-12-25 20:44:29 +0800

    Add pictures to fill up spots

--- body/src/proj/ProjBody.js
+++ body/src/proj/ProjBody.js
@@ -2,6 +2,12 @@ import './ProjBody.css';
 import epp1 from './epp1.jpg';
 import epp2 from './epp2.jpg';
 import turtlebot from './turtlebot.jpg';
+import psa from './psa.png';
+import ideate from './ideate.jpg';
+import orbital from './orbital.png';
+import rtos from './rtos.png';
+import wiagi from './wiagi.png';
+import rpg from './rpg.png';
 
 function ProjBody() {
   return (
@@ -27,12 +33,30 @@ function ProjBody() {
           <img src={turtlebot} className='turtlebot'></img>
           <div className='blur'></div>
         </div>
-        <div className='proj-container'></div>
-        <div className='proj-container'></div>
-        <div className='proj-container'></div>
-        <div className='proj-container'></div>
-        <div className='proj-container'></div>
-        <div className='proj-container'></div>
+        <div className='proj-container'>
+          <img src={psa} className='psa'></img>
+          <div className='blur'></div>
+        </div>
+        <div className='proj-container'>
+          <img src={ideate} className='ideate'></img>
+          <div className='blur'></div>
+        </div>
+        <div className='proj-container'>
+          <img src={orbital} className='orbital'></img>
+          <div className='blur'></div>
+        </div>
+        <div className='proj-container'>
+          <img src={rtos} className='rtos'></img>
+          <div className='blur'></div>
+        </div>
+        <div className='proj-container'>
+          <img src={wiagi} className='wiagi'></img>
+          <div className='blur'></div>
+        </div>
+        <div className='proj-container'>
+          <img src={rpg} className='rpg'></img>
+          <div className='blur'></div>
+        </div>
       </div>
     </div>
   )
